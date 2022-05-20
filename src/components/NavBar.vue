94fb4842232fa24bce7e4aed810b246ebe833f11
ratc00 <ratrace00@hotmail.com>
2022-05-20 15:18:43 +0800

    2nd commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <v-toolbar app color="primary">
+            <v-toolbar-title class="text-uppercase white--text">
+                <span class="font-weight-light">My</span>
+                <span>Project</span>
+            </v-toolbar-title>
+            <v-btn color="blue lighten-4">
+                <span>Sign Out</span>
+                <v-icon right>exit-to-app</v-icon>
+            </v-btn>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+export default{
+
+}
+</script>
